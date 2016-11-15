80af6a57a79a67b5551503af160f54f8b251056d
tbeu <tbeu@users.noreply.github.com>
2016-11-15 22:10:16 +0100

    Test performance on Travis CI

--- .install_hdf5_1_8.sh
+++ .install_hdf5_1_8.sh
@@ -2,5 +2,5 @@
 
 git clone --branch hdf5_1_8_18 https://bitbucket.hdfgroup.org/scm/hdffv/hdf5.git hdf5_1_8_18
 cd hdf5_1_8_18
-./configure --quiet --enable-shared --disable-production --enable-debug=all --with-pic --disable-deprecated-symbols --disable-hl --disable-strict-format-checks --disable-clear-file-buffers --disable-instrument --disable-parallel --disable-trace --with-default-api-version=v18 CFLAGS="-w"
+./configure --quiet --enable-shared --enable-production=yes --enable-debug=no --with-pic --disable-deprecated-symbols --disable-hl --disable-strict-format-checks --disable-clear-file-buffers --disable-instrument --disable-parallel --disable-trace --with-default-api-version=v18 CFLAGS="-O3 -w"
 make install -C src
