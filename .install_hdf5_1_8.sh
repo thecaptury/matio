1a21d0ede2e4292bbe1eadb8a6616ead5f7bc1d9
tbeu <tbeu@users.noreply.github.com>
2016-11-17 20:39:33 +0100

    Tune HDF5 configuration

--- .install_hdf5_1_8.sh
+++ .install_hdf5_1_8.sh
@@ -2,5 +2,5 @@
 
 svn co -q https://svn.hdfgroup.org/hdf5/branches/hdf5_1_8_17@29907
 cd hdf5_1_8_17
-./configure --quiet --enable-shared --disable-production --enable-debug=all --with-pic --disable-deprecated-symbols --disable-hl --disable-strict-format-checks --disable-clear-file-buffers --disable-instrument --disable-parallel --disable-trace --with-default-api-version=v18 CFLAGS="-w"
+./configure --quiet --enable-shared --disable-production --enable-debug=all --with-pic --disable-deprecated-symbols --disable-hl --enable-strict-format-checks --enable-clear-file-buffers --enable-instrument --disable-parallel --enable-trace --with-default-api-version=v18 CFLAGS="-w"
 make install -C src
