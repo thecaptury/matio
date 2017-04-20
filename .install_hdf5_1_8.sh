3f7e5b8af23707db2ff3c209ab98d083f86e446d
tbeu <tc@tbeu.de>
2017-04-20 20:39:13 +0200

    Build hdf5 with szlib on Travis CI
    As reported by https://github.com/tbeu/matio/issues/59

--- .install_hdf5_1_8.sh
+++ .install_hdf5_1_8.sh
@@ -1,6 +1,12 @@
 #! /bin/sh 
 
-git clone --branch hdf5_1_8_18 https://bitbucket.hdfgroup.org/scm/hdffv/hdf5.git hdf5_1_8_18
+curl https://support.hdfgroup.org/ftp/lib-external/szip/2.1.1/src/szip-2.1.1.tar.gz -O -J
+tar -zxf szip-2.1.1.tar.gz
+cd szip-2.1.1
+./configure --quiet --enable-static=no CFLAGS="-w"
+make install -C src
+cd ..
+git clone --quiet --depth 1 --branch hdf5_1_8_18 https://bitbucket.hdfgroup.org/scm/hdffv/hdf5.git hdf5_1_8_18
 cd hdf5_1_8_18
-./configure --quiet --enable-shared --disable-production --enable-debug=all --with-pic --disable-deprecated-symbols --disable-hl --disable-strict-format-checks --disable-clear-file-buffers --disable-instrument --disable-parallel --disable-trace --with-default-api-version=v18 CFLAGS="-w"
+./configure --enable-shared --disable-production --enable-debug=all --with-pic --disable-deprecated-symbols --disable-hl --disable-strict-format-checks --disable-clear-file-buffers --disable-instrument --disable-parallel --disable-trace --with-default-api-version=v18 --with-szlib=$TRAVIS_BUILD_DIR/szip-2.1.1/szip CFLAGS="-w"
 make install -C src
